7da8bf67ad164ad20971fd292751424825969a7a
stvogt <stvogtgeisse@gmail.com>
2023-01-09 19:37:09 -0300

    added inputs

---
+++ inputs/relax_scan.sh
@@ -0,0 +1,22 @@
+/home/svogt/repos/QCScan/relax_new.py --client_address 152.74.10.245:7778 \
+    --username ''\
+    --password "" \
+    --starting-geometry "38551" \
+    --scan-coordinate "35 43" \
+    --scan-type "distance" \
+    --base-name "salen-OMe_d-lac_i1_0002" \
+    --scan-level-of-theory "pbe0_def2-svp" \
+    --energy-level-of-theory "pbe0-d3bj_def2-tzvp" \
+    --initial-coordinate-value 0 \
+    --final-coordinate-value 1.8 \
+    --increment -0.05 \
+    --entry-rounding 2 \
+    --geometric-options '{"maxiter" : 150}' \
+    --gradient-options '{"dftd": "d3"}' \
+    --program "terachem" \
+    --en_program "psi4" \
+    --cores 1 \
+    --memory 24 \
+    --tag "rx_comp" \
+    $1 \
+    $2 \
---
+++ inputs/sample_site.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+python /home/svogt/repos/beep/scripts/launch_site_sampling.py --client-address 152.74.10.245:7778 \
+     --cluster-name salen-OMe  \
+     --cluster-collection initiators   \
+     --sampling-molecule-name l-lactide \
+     --sampling-molecule-collection monomers \
+     --molecule-size 5 \
+     --number-of-rounds 3 \
+     --sampling-shell 3.0 \
+     --zenith-angle 1.6 \
+     --maximal-binding-sites 15 \
+     --level-of-theory pbe0-d3_def2-svp \
+     --refinement-level-of-theory pbe0-d3_def2-svp \
+     --rmsd-value 0.15 \
+     --program terachem \
+     --sampling-tag tera \
+     --noise \
+     --purge 1.0 \
+     #--username None \
+     #--password None  \
+     #--grid-size sparse
+    # --keyword_id 1  \
+    #--rmsd_symmetry
+    #--level_of_theory upbe0-d3_def2-svp \
+
