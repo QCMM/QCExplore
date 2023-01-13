a05bdc5381f61b23195c4319f911c8fd21a7666e
stvogt <stvogtgeisse@gmail.com>
2023-01-13 15:47:36 -0300

    add scan sub

--- inputs/relax.sh
+++ inputs/relax.sh
@@ -1,21 +1,22 @@
-/opt/shared/QCScan/relax.py --client_address 152.74.10.245:7777 \
-    --username 'svogt'\
-    --password "7kyRT-Mrow3jH0Lg6b9YIhEjAcvU9EpFBb9ouMClU5g" \
-    --starting-geometry "/home/svogt/fractal/catalisis/w2.xyz" \
-    --scan-coordinate "1 11" \
+/home/svogt/repos/QCScan/relax_new.py --client_address 152.74.10.245:7778 \
+    --username ''\
+    --password "" \
+    --starting-geometry "38551" \
+    --scan-coordinate "35 43" \
     --scan-type "distance" \
-    --base-name "NH2CO+H2O--NH2CHO+OH" \
-    --optimization-level-of-theory "b3lyp-d3bj_def2-svp" \
-    --scan-level-of-theory "pbe-d3bj_def2-svp" \
-    --energy-level-of-theory "hf3c_minix" \
+    --base-name "salen-OMe_d-lac_i1_0002" \
+    --scan-level-of-theory "pbe0_def2-svp" \
+    --energy-level-of-theory "pbe0-d3bj_def2-tzvp" \
     --initial-coordinate-value 0 \
-    --final-coordinate-value 1.1 \
+    --final-coordinate-value 1.8 \
     --increment -0.05 \
     --entry-rounding 2 \
     --geometric-options '{"maxiter" : 150}' \
-    --gradient-options {} \
-    --program "psi4" \
-    --cores 4 \
-    --memory 16 \
-    --tag "refinement" \
-
+    --gradient-options '{"dftd": "d3"}' \
+    --program "terachem" \
+    --en_program "psi4" \
+    --cores 1 \
+    --memory 24 \
+    --tag "rx_comp" \
+    $1 \
+    $2 \
